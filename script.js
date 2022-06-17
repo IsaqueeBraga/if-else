c66ab4c1c89c9dc4770ee25ef9b37eb128de6fb5
Isaquee Braga <flaisaque@hotmail.com>
2022-06-16 23:28:26 -0300

    aula02

---
+++ script.js
@@ -0,0 +1,31 @@
+let idade = 18
+
+
+if(idade>= 18){
+    console. log ('acesso permitido')
+}else{
+    console. log ('acesso negado')
+}
+if(idade>=18 === true){
+console. log (true)
+}else{
+    console. log(false)
+}
+
+
+let mesAniversario = 01
+let outroMes = 12
+if(mesAniversario == 01 || outroMes !== 12){
+    console. log ('Voce faz aniversario em janeiro.')
+}else{
+    console. log('Voce faz aniversario em dezembro.')
+}
+
+
+let letraR = true
+if(letraR){
+    console. log('começa com a letra R')
+}else{
+    console. log ('não começa com a letra R')
+}
+
